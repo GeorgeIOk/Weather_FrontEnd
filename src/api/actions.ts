c0a252307543b4ad5a24fc58345799198cf9f7e8
Billal2141884 <161714794+Billal2141884@users.noreply.github.com>
2024-03-07 11:44:30 +0000

    URL changed and Tested

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://friendly-fishstick-5gv44vrw5v6pc4xvr-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
